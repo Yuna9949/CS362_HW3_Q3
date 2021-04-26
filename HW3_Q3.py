843b8701b016b564c7471fb0c44f7678ffa10f78
Yuna Oh <ustina0409@gmail.com>
2021-04-26 00:29:34 -0700

    Leap year code with error handling

--- HW3_Q3.py
+++ HW3_Q3.py
@@ -2,8 +2,22 @@
 #934-371-471 Yuna Oh
 #Input year and press enter. Then Output will be printed.
 
-year = int(input('Input year : '))
+class MyError(Exception):
+    pass
 
+def number_chk(yr):
+    if not yr.isdecimal():
+        raise MyError()
+
+y = input('Input year : ')
+
+try:
+    number_chk(y)
+except MyError:
+    print("It's not a number")
+    exit()
+
+year = int(y)
 if year % 4 == 0:
     if year % 100 == 0:
         if year % 400 == 0:
